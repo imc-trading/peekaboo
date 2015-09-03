bb99d8e16723c98070c3f4562880bb9aad79ce78
Michael Persson <michael.ake.persson@gmail.com>
2015-09-03 16:25:48 +0200

    Changed rel for brew

--- peekaboo.rb
+++ peekaboo.rb
@@ -1,8 +1,8 @@
 class Peekaboo < Formula
-  version "0.2.1"
+  version "0.2.2"
   homepage "https://github.com/mickep76/peekaboo"
   url "https://github.com/mickep76/peekaboo/archive/#{version}.tar.gz"
-  sha256 "3ad8d25fafbbb730509b49bef94be4994d58c4725efd0189eab3cf959e6a3324"
+  sha256 "0dbfe933dc4257ea1659bac6f27582d5cb295281132e159c9fe972e3f49bf982"
 
   depends_on "go" => :build
 
