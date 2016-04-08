9c10b36858f65e71df8904124d72858804767540
Michael Persson <michael.ake.persson@gmail.com>
2016-04-08 09:11:40 -0500

    Added kernel config

---
+++ src/github.com/imc-trading/peekaboo/system/opsys/kernelcfg/kernelcfg.go
@@ -0,0 +1,55 @@
+// +build linux
+
+package kernelcfg
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/imc-trading/peekaboo/parse"
+)
+
+type KernelCfgs []KernelCfg
+
+type KernelCfg struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+func Get() (KernelCfgs, error) {
+	kernel, err := parse.Exec("uname", []string{"-r"})
+	if err != nil {
+		return KernelCfgs{}, err
+	}
+
+	fn := "/boot/config-" + kernel
+	if _, err := os.Stat(fn); os.IsNotExist(err) {
+		return KernelCfgs{}, fmt.Errorf("file doesn't exist: %s", fn)
+	}
+
+	o, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return KernelCfgs{}, err
+	}
+
+	k := KernelCfgs{}
+	for _, line := range strings.Split(string(o), "\n") {
+		v := strings.Split(line, "=")
+		if len(v) < 2 {
+			continue
+		}
+
+		k = append(k, KernelCfg{
+			Key:   v[0],
+			Value: v[1],
+		})
+	}
+
+	return k, nil
+}
+
+func GetInterface() (interface{}, error) {
+	return Get()
+}
