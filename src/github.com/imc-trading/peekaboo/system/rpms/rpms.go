ae7a24fbf5deb25216e4acfa59051d10c62d120a
Michael Persson <michael.ake.persson@gmail.com>
2016-04-08 10:52:23 -0500

    Fix timestamp for installed rpm

--- src/github.com/imc-trading/peekaboo/system/rpms/rpms.go
+++ src/github.com/imc-trading/peekaboo/system/rpms/rpms.go
@@ -3,7 +3,9 @@
 package rpms
 
 import (
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/imc-trading/peekaboo/parse"
 )
@@ -11,14 +13,15 @@ import (
 type RPMs []RPM
 
 type RPM struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
-	Release string `json:"release"`
-	Arch    string `json:"arch"`
+	Name      string `json:"name"`
+	Version   string `json:"version"`
+	Release   string `json:"release"`
+	Arch      string `json:"arch"`
+	Installed string `json:"installed"`
 }
 
 func Get() (RPMs, error) {
-	o, err := parse.Exec("rpm", []string{"-qa", "--queryformat", "%{NAME}|%{VERSION}|%{RELEASE}|%{ARCH}\n"})
+	o, err := parse.Exec("rpm", []string{"-qa", "--queryformat", "%{NAME}|%{VERSION}|%{RELEASE}|%{ARCH}|%{INSTALLTIME}\n"})
 	if err != nil {
 		return RPMs{}, err
 	}
@@ -26,15 +29,23 @@ func Get() (RPMs, error) {
 	r := RPMs{}
 	for _, line := range strings.Split(string(o), "\n") {
 		v := strings.Split(line, "|")
-		if len(v) < 4 {
+		if len(v) < 5 {
 			continue
 		}
 
+		sec, err := strconv.ParseInt(v[4], 10, 64)
+		if err != nil {
+			return RPMs{}, err
+		}
+
+		tm := time.Unix(int64(sec), 0)
+
 		r = append(r, RPM{
-			Name:    v[0],
-			Version: v[1],
-			Release: v[2],
-			Arch:    v[3],
+			Name:      v[0],
+			Version:   v[1],
+			Release:   v[2],
+			Arch:      v[3],
+			Installed: tm.Format("2006-01-02T15:04:05Z"),
 		})
 	}
 
